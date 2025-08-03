6520bfadc9e3bec738d3999dac300fc72652df73
surajsln570 <surajsln570@gmail.com>
2025-08-03 22:05:43 +0530

    a

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-let menu=document.querySelector("#menu")
-let menuicon=document.querySelector("#i")
-function handleMenu(){
-    if(menu.style.display===""){
-        menu.style.display="block"
-        menuicon.innerHTML=`<i onclick="handleMenu()" class="fa-solid fa-xmark text-white my-auto text-4xl mx-5 cursor-pointer" aria-hidden="true"></i>`
+let menu = document.querySelector("#menu")
+let menuicon = document.querySelector("#i")
+let box1 = document.querySelector("#box1")
+// console.log(box1.parentElement.innerHTML)
+
+function handleMenu() {
+    // console.log("handleMenu")
+    if (menu.style.display === "") {
+        // console.log("block")
+        menu.style.display = "block"
+        menuicon.innerHTML = ``
     }
-    else{
-        menu.style.display=""
-        menuicon.innerHTML=`<i onclick="handleMenu()" class="fa-solid fa-bars text-white my-auto text-4xl mx-5 cursor-pointer" aria-hidden="true"></i>`
+    else {
+        // console.log("hidden")
+        menu.style.display = ""
+        menuicon.innerHTML = `<i class="fa-solid fa-bars text-white my-auto text-4xl mx-5 cursor-pointer"></i>`
     }
 }
+
+function handleMenu1() {
+    console.log("section")
+    menu.style.display = ""
+    menuicon.innerHTML = `<i class="fa-solid fa-bars text-white my-auto text-4xl mx-5 cursor-pointer" aria-hidden="true"></i>`
+
+}
+
+
